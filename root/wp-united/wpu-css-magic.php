338be8c099332019048c06bef0cbc10b392dda33
John Wells <jfw@jfwhome.com>
2009-05-31 10:45:43 +0000

    NEW "CSS Magic" class to aid with template integration. The idea is that phpBB and/or WordPress styles can be modified on-the-fly to make template integration "magically work", even for templates we know nothing about.
    This is prototype code -- untested, will need some fixing. Usage notes are included as this class should be useful for other people wanting to do template integrations with various CSS files of unknown origin.
    
    git-svn-id: http://www.wp-united.com/svn/trunk@105 c1c82642-cce3-494f-bf0e-9d5eb4df7846

---
+++ root/wp-united/wpu-css-magic.php
@@ -0,0 +1,220 @@
+<?php
+/** 
+*
+* WP-United "CSS Magic" template integrator
+*
+* @package WP-United
+* @version $Id: wp-united.php,v0.9.5[phpBB2]/v 0.7.1[phpBB3] 2009/05/18 John Wells (Jhong) Exp $
+* @copyright (c) 2006-2009 wp-united.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
+
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+//
+//
+//	------------------------------
+// This experimental library attempts to magically fuse templates on the page
+// by modifying CSS specicifity on the fly
+//
+// Inspired by cssParser class. you are welcome to use this class in your own projects, provided this licence is kept intact.
+// John Wells, 2009
+// --------------------------------
+//
+/* USAGE NOTES
+	
+	// instantiate CSS Magic
+	$cssMagic = CSS_Magic::getInstance();
+	
+	// to read a css file. 
+	$success = $cssMagic->parseFile('path_to_file');
+	
+	// or read a css string
+	$cssMagic->clear(); // you can read multiple strings and files. They are combined together. To start anew, use this.
+	$success = $cssMagic->parseString($cssString);
+	
+	// $success will return 1 or true on success, or 0 or false on failure.
+	
+	//Now, make all the CSS we have read in apply only to children of a particular DIV with ID = $id
+	$cssMagic->makeSpecific($id);
+	
+	// Now get the modified CSS. The output is fairly nicely compressed too.
+	$fixedCSS = $cssMagic->getCSS();
+	
+	// alternatively, send the output straight to the browser as a CSS file
+	$cssMagic->sendCSS();
+	
+	// When you're finished,
+	$cssmagic->clear();
+	
+	// Note: CSS Magic doesn't try to validate the CSS coming in. If the inbound CSS in invalid or garbage, 
+	// you'll get garbage coming out -- perhaps even worse than before.
+	
+	// (c) John Wells, 2009
+	
+*/
+
+if ( !defined('IN_PHPBB') && !defined('ABSPATH') )
+{
+	die("Hacking attempt");
+	exit;
+}
+
+
+class CSS_Magic {
+	var $css;
+	var $filename;
+	
+	//
+	//	GET INSTANCE
+	//	----------------------
+	//	Makes class a Singleton.
+	//	
+	function getInstance () {
+		static $instance;
+		if (!isset($instance)) {
+			$instance = new CSS_Magic();
+        } 
+        return $instance;
+    }
+	
+	// 
+	// 	CLASS CONSTRUCTOR
+	//	------------------------------
+	//	
+	//	
+	function CSS_Magic() {
+		$this->clear();
+		$this->filename = '';
+	}
+	
+	function clear() {
+		$this->css = array();	
+	}
+
+	// 
+	// 	PARSE STRING
+	//	------------------------------
+	//	Parses raw CSS, storing the keys and CSS code. We don't separate or process the keys, we don't need to.
+	//	
+	function parseString($str) {
+		$this->Clear();
+		// Remove comments
+		$str = preg_replace("/\/\*(.*)?\*\//Usi", "", $str);
+	
+		$parts = explode("}",$str);
+		if(count($parts) > 0) {
+			foreach($parts as $part) {
+				list($keys,$cssCode) = explode("{",$part);
+				// store full selector
+				if(strlen($keys) > 0) {
+					$keys = str_replace("\n", "", $keys);
+					$keys = str_replace("\\", "", $keys);
+					$this->addSelector($keys, trim($cssCode));
+				}
+			}
+		}
+		return (count($this->css) > 0);
+	}
+	// 
+	// 	PARSE FILE
+	//	------------------------------
+	//	Opens and parses the CSS file.
+	//	  
+	function parseFile($filename, $clear = false;) {
+		if ($clear) $this->clear();
+		$this->filename = $filename;
+		if(file_exists($filename)) {
+			parseString(file_get_contents($filename));
+		} else {
+			return false;
+		}
+	}
+	// 
+	// 	ADD SELECTOR
+	//	------------------------------
+	//	Stores a full CSS selector in our class. Combines identical selectors together
+	//	if CSS is passed in in the order it should appear on the page, this shouldn't affect specicifity	
+	function addSelector($keys, $cssCode) {
+		$keys = trim($keys);
+		$cssCode = trim($cssCode);
+		if(!isset($this->css[$keys])) {
+			$this->css[$keys] = $cssCode;
+		} else {
+			$this->css[$keys] = str_replace(';;', ';', $this->css[$keys] . ';' . $cssCode);
+		}
+	}
+	// 
+	// 	MAKE SPECIFIC
+	//	------------------------------
+	//	Makes all stored CSS specific to a particular parent ID
+	//
+	function makeSpecific($id) {
+		$fixed = array();
+		// things that could be delimiting a "body" selector at the beginning of our string.
+		$seps = array(' ', '>', '<', '.', '#');
+		foreach($this->css as $keyString => $cssCode) {
+			$fixedKeys = array();
+			$keys = explode(',' $keyString);
+			foreach($keys as $key) {
+				$fixedKey = trim($key);
+				$foundBody = false;
+				// remove references to 'body'
+				foreach($seps as $sep) {
+					$keyElements = explode($sep, $fixedKey);
+					if(strtolower($keyElements[0]) == "body") {
+						$keyElements[0] = "#{$id}";
+						$fixedKey = implode($sep, $keyElements);
+						$foundBody = true;
+					}
+				}
+				// add #id selector before each selector
+				if(!$foundBody) {
+					$fixedKey = "#{$id} " . $fixedKey;
+				}
+				$fixedkeys[] = $fixedkey;
+			}
+			// recreate the fixed key
+			$fixedKeyString = implode(', ', $fixedkeys);
+						
+			if(!isset($this->fixed[$fixedKeyString])) {
+				$this->fixed[$fixedKeyString] = $cssCode;
+			} else {
+				$this->fixed[$fixedKeyString] = str_replace(';;', ';', $this->fixed[$fixedKeyString] . ';' . $cssCode);
+			}
+		}
+		// done
+		$this->css = $this->fixed;
+		unset($this->fixed);
+
+	}
+
+	// 
+	// 	GET CSS
+	//	------------------------------
+	//	Outputs all our stored, fixed (hopeffuly!) CSS
+	//	
+	function getCSS() {
+		$response = '';
+		foreach($this->css as $keyString => $cssCode) {
+			$response .= $keyString . '{' . $cssCode . '}';
+		}
+		return $response;
+	}
+	
+	function sendCSS() {
+		header("Content-type: text/css");
+		echo $this->getCSS();
+	
+	}
+}
+
+
+?>
