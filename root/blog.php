5111a3fd0e14033ea62f7bb9bd652a5972982cb4
John Wells <jfw@jfwhome.com>
2009-06-06 18:18:00 +0000

    earlier code added to blog.php was a red herring -- the session instantiation is already taking place in abstractify.php. We need to look elsewhere for the autologin problems.
    git-svn-id: http://www.wp-united.com/svn/trunk@115 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- root/blog.php
+++ root/blog.php
@@ -70,14 +70,6 @@ if (file_exists($phpbb_root_path . 'common.' . $phpEx)) {
 
 	require_once($phpbb_root_path . 'wp-united/abstractify.' . $phpEx);	
 	
-	
-	if ( 'PHPBB3' == $wpuAbs->ver ) {
-			$user->session_begin();
-			$auth->acl($user->data);
-			$user->setup();
-	}
-	
-
 	define('WPU_BLOG_PAGE', 1);
 
 	require_once($phpbb_root_path . 'wp-united/mod-settings.' . $phpEx);
