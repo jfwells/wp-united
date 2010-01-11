873806ce3c803db3adc1fc25fb0fe8477f80633f
John Wells <jfw@jfwhome.com>
2010-01-11 05:06:22 +0000

    Fixed broken wpu-install.php
    git-svn-id: http://www.wp-united.com/svn/trunk@242 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- root/wpu-install.php
+++ root/wpu-install.php
@@ -19,6 +19,10 @@ $phpbb_root_path = './';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup('mods/wp-united');
+
 require($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
 require($phpbb_root_path . 'includes/functions_module.' . $phpEx);
 
