e07dd80591bf4ee6b23f26367eb50d07fb3c4bc7
John Wells <jfw@jfwhome.com>
2010-08-24 09:25:55 +0000

    -- Fixed error in install.xl -- Fixed notices in filetree install box
    git-svn-id: http://www.wp-united.com/svn/trunk@544 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- plugin/wp-united/js/filetree.php
+++ plugin/wp-united/js/filetree.php
@@ -24,27 +24,29 @@ $docRoot =  (isset($_SERVER['DOCUMENT_ROOT'])) ? $_SERVER['DOCUMENT_ROOT'] : str
 $docRoot = @realpath($docRoot); 
 $docRoot = ($docRoot[strlen($docRoot)-1] == "/" ) ? $docRoot : $docRoot . "/";
 
-if(stristr($_POST['dir'], $docRoot) === false) {
-	$_POST['dir'] = $docRoot . urldecode($_POST['dir']);
-	$_POST['dir'] = str_replace('//', '/', $_POST['dir']);
+$fileLoc = $_POST['dir'];
+
+if(stristr($fileLoc, $docRoot) === false) {
+	$fileLoc = $docRoot . urldecode($fileLoc);
+	$fileLoc = str_replace('//', '/', $fileLoc);
 }
 
-if( file_exists($_POST['dir']) ) {
-	$files = scandir($root . $_POST['dir']);
+if( file_exists($docRoot) ) {
+	$files = scandir($fileLoc);
 	natcasesort($files);
 	if( count($files) > 2 ) { /* The 2 accounts for . and .. */
 		echo "<ul class=\"jqueryFileTree\" style=\"display: none;\">";
 		// All dirs
 		foreach( $files as $file ) {
-			if( file_exists($root . $_POST['dir'] . $file) && $file != '.' && $file != '..' && is_dir($root . $_POST['dir'] . $file) ) {
-				echo "<li class=\"directory collapsed\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "/\">" . htmlentities($file) . "</a></li>";
+			if( file_exists($fileLoc. $file) && $file != '.' && $file != '..' && is_dir($fileLoc . $file) ) {
+				echo "<li class=\"directory collapsed\"><a href=\"#\" rel=\"" . htmlentities($fileLoc . $file) . "/\">" . htmlentities($file) . "</a></li>";
 			}
 		}
 		// All files
 		foreach( $files as $file ) {
-			if( file_exists($root . $_POST['dir'] . $file) && $file != '.' && $file != '..' && !is_dir($root . $_POST['dir'] . $file) ) {
+			if( file_exists($fileLoc . $file) && $file != '.' && $file != '..' && !is_dir($fileLoc . $file) ) {
 				$ext = preg_replace('/^.*\./', '', $file);
-				echo "<li class=\"file ext_$ext\"><a href=\"#\" rel=\"" . htmlentities($_POST['dir'] . $file) . "\">" . htmlentities($file) . "</a></li>";
+				echo "<li class=\"file ext_$ext\"><a href=\"#\" rel=\"" . htmlentities($fileLoc . $file) . "\">" . htmlentities($file) . "</a></li>";
 			}
 		}
 		echo "</ul>";	
