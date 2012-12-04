1f75a2b2701bb73d57f152748ebbbfd5b35a32d7
John Wells <jfw@jfwhome.com>
2012-12-04 05:30:47 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@814 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- WordPress Plugin/wp-united/settings-panel.php
+++ WordPress Plugin/wp-united/settings-panel.php
@@ -151,7 +151,7 @@ function wpu_advanced_options() {
 		<img id="panellogo" src="<?php echo $wpUnited->get_plugin_url() ?>images/settings/seclogo.jpg" />
 		<?php screen_icon('options-general'); ?>
 		<h2> <?php _e('WP-United Advanced Options'); ?> </h2>
-		<p><?php echo sprintf(__('Some additional options can be set in the included file, %s1. These do not normally need to be changed. However, to review and change these options, please open %s1 in a text editor.'), add_trailing_slash($wpUnited->get_plugin_path()) . 'options.php'); ?></p>
+		<p><?php echo sprintf(__('Some additional options can be set in the included file, %1s. These do not normally need to be changed. However, to review and change these options, please open %1s in a text editor.'), add_trailing_slash($wpUnited->get_plugin_path()) . 'options.php'); ?></p>
 
 		<?php
 		if(isset($_POST['wpuadvanced-submit'])) {
