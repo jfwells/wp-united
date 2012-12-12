2a977cdaddf921c4b6301a0b2cc04a1e4836d661
John Wells <jfw@jfwhome.com>
2012-12-12 10:56:43 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@989 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- WordPress Plugin/wp-united/widgets.php
+++ WordPress Plugin/wp-united/widgets.php
@@ -537,7 +537,7 @@ class WPU_Forum_Nav_Block_Widget extends WP_Widget {
 	public function add_navblock_style() {
 		global $phpbbForum, $phpEx, $wpUnited;
 		
-		$ssLink = $phpbbForum->get_board_url() . 'style.' . $phpEx . '?id=' . $phpbbForum->get_userdata('user_id') . '&usecssm=1&wpunav=1';
+		$ssLink = $phpbbForum->get_board_url() . 'style.' . $phpEx . '?id=' . $phpbbForum->get_userdata('user_id') . '&lang=en&usecssm=1&wpunav=1';
 		wp_register_style('wpuPhpbbNavReset', $wpUnited->get_plugin_url() . 'theme/reset.css');
 		wp_register_style('wpuPhpbbNavStyle', $ssLink);
 
