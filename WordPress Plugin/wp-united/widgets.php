63244e9f358705aa0a2f2f5abe82eaaf6afe5d2d
John Wells <jfw@jfwhome.com>
2012-12-14 09:17:45 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1055 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- WordPress Plugin/wp-united/widgets.php
+++ WordPress Plugin/wp-united/widgets.php
@@ -738,6 +738,7 @@ class WPU_Forum_Polls_Widget extends WP_Widget {
 				function wpu_poll_submit(pollID, el) {
 					
 					var $el = $wpuPoll(el).parents('.wpupoll-' + pollID).find('form :first');
+					$el.find('input').attr('disabled', 'disabled');
 					var formData = $el.serialize() +'&wpupoll=1&pollid=' + pollID + '&ajax=1&display=0&_ajax_nonce=' + wpuPollNonce;
 					
 					$wpuPoll.post('<?php echo $wpUnited->get_wp_home_url(); ?>', formData, function(response) {
@@ -745,7 +746,7 @@ class WPU_Forum_Polls_Widget extends WP_Widget {
 						wpuPollNonce = $response.find('newnonce').text();
 						var newContent = Base64.decode($response.find('markup').text());
 						var updateID = $response.find('pollid').text();
-						alert(updateID + '|' + wpuPollNonce + '|' + newContent);
+						
 						$wpuPoll('.wpupoll-' + updateID).html(newContent);
 					});
 					
