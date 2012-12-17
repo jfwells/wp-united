a43eeb9cef1eb8b60502a79d8448d59288895d21
John Wells <jfw@jfwhome.com>
2012-12-17 09:32:26 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1098 c1c82642-cce3-494f-bf0e-9d5eb4df7846

---
+++ phpbb3 MOD/root/wp-united/version.php
@@ -0,0 +1,25 @@
+<?php
+
+/** 
+*
+* WP-United -- version setting
+*
+* @package WP-United
+* @version $Id: v0.9.0.3 2012/12/06 John Wells (Jhong) Exp $
+* @copyright (c) 2006-2012 wp-united.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License  
+* @author John Wells
+*
+* THIS FILE MUST HAVE NO CLOSING ?> AS THE WP-UNITED BUILD SYSTEM CAN APPEND CONTENT TO THIS FILE!
+*/
+
+
+if ( !defined('IN_PHPBB') && !defined('ABSPATH') ) {
+	exit;
+}
+
+
+global $wpuVersion_phpbb;
+$wpuVersion_phpbb = '0.9.1.0';
+
+
