7737121117519e184bdf103a1717db247a7b395b
John Wells <jfw@jfwhome.com>
2012-12-28 06:04:50 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1216 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- WordPress Plugin/wp-united/settings-panel.php
+++ WordPress Plugin/wp-united/settings-panel.php
@@ -1304,16 +1304,7 @@ function wpu_settings_page() {
 										
 										<option value="0"<?php if($wpUnited->get_setting('wpSimpleHdr') == 1) { echo ' selected="selected" '; } ?>>-- <?php _e('Simple Header &amp; Footer (recommended)', 'wp-united'); ?> --</option>
 										<?php
-											$files = array();
-											$parentFiles = WP_Theme::scandir(TEMPLATEPATH, 'php', 1);
-											$childFiles = WP_Theme::scandir(STYLESHEETPATH, 'php', 1);
-											
-											if(is_array($childFiles) && is_array($parentFiles)) {
-												$files = array_merge($parentFiles, $childFiles);
-											}
-											
-											sort($files);
-	
+											$files = get_page_templates();
 											
 											if(sizeof($files)) {
 												foreach($files as $file) {
