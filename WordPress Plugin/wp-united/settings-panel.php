c5e556afbb2d50794aa8a939ba5e3c5167882bda
John Wells <jfw@jfwhome.com>
2012-12-31 07:30:07 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1250 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- WordPress Plugin/wp-united/settings-panel.php
+++ WordPress Plugin/wp-united/settings-panel.php
@@ -420,7 +420,7 @@ function wpu_setup_menu() {
 			<div id="phpbbpath" style="display: none;">&nbsp;</div>
 			<p id="wpubackupgroup" style="display: none;"><strong><span id="phpbbdocroot"><?php echo $docRoot; ?><input type="text" id="wpubackupentry" name="wpubackupentry" value="<?php echo $showBackupPath; ?>"></span></input>/config.php</strong></p>
 		</div>
-		<p>Path selected: <strong id="phpbbpathshow" style="color: red;"><?php _e('Not selected', 'wp-united'); ?></strong> <a id="phpbbpathchooser" href="#" onclick="return wpuChangePath();" style="display: none;"><?php _e('Change Location &raquo;', 'wp-united'); ?></a><a id="wpucancelchange" style="display: none;" href="#" onclick="return wpuCancelChange();"><?php _e('Cancel Change', 'wp-united'); ?></a></p>
+		<p><?php _e('Path selected: ', 'wp-united'); ?><strong id="phpbbpathshow" style="color: red;"><?php _e('Not selected', 'wp-united'); ?></strong> <a id="phpbbpathchooser" href="#" onclick="return wpuChangePath();" style="display: none;"><?php _e('Change Location &raquo;', 'wp-united'); ?></a><a id="wpucancelchange" style="display: none;" href="#" onclick="return wpuCancelChange();"><?php _e('Cancel Change', 'wp-united'); ?></a></p>
 		<input id="wpupathfield" type="hidden" name="wpu-path" value="notset"></input>
 	
 		<p class="submit">
