28ff598f890f65cd3074cff855cf7f583b589d99
John Wells <jfw@jfwhome.com>
2012-12-31 05:36:57 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1237 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- WordPress Plugin/wp-united/settings-panel.php
+++ WordPress Plugin/wp-united/settings-panel.php
@@ -416,7 +416,7 @@ function wpu_setup_menu() {
 			
 
 		?>
-		<p><strong><?php echo $docRoot; ?><input type="text" id="wpubackupentry" style="name="wpubackupentry" value="<?php echo $showBackupPath;"></input>/config.php</strong></p>
+		<p><strong><?php echo $docRoot; ?><input type="text" id="wpubackupentry" style="name="wpubackupentry" value="<?php echo $showBackupPath; ?>"></input>/config.php</strong></p>
 		<p>Path selected: <strong id="phpbbpathshow" style="color: red;"><?php _e('Not selected', 'wp-united'); ?></strong> <a id="phpbbpathchooser" href="#" onclick="return wpuChangePath();" style="display: none;"><?php _e('Change Location &raquo;', 'wp-united'); ?></a><a id="wpucancelchange" style="display: none;" href="#" onclick="return wpuCancelChange();"><?php _e('Cancel Change', 'wp-united'); ?></a></p>
 		<input id="wpupathfield" type="hidden" name="wpu-path" value="notset"></input>
 	
