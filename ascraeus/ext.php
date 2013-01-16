96127ff76514f1964ccb0973d36e3b28a90ee0d6
John Wells <jfw@jfwhome.com>
2013-01-16 06:49:18 +0000

    More ascraeus
    git-svn-id: http://www.wp-united.com/svn/trunk@1458 c1c82642-cce3-494f-bf0e-9d5eb4df7846

---
+++ ascraeus/ext.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Can override phpBB's installation / uninstallation of extension here.
+ * Base class is in includes/extension/base.php, interface.php
+*/
+class phpbb_ext_wp-united extends phpbb_extension_base
+{
+}
