99a9e7bbe2fb97119105bc4748125daa12dcb586
John Wells <jfw@jfwhome.com>
2013-01-17 11:20:42 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1495 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- unit_tests/boot_test.php
+++ unit_tests/boot_test.php
@@ -1,7 +1,7 @@
 <?
 define('WPU_TEST_ROOT_PATH', realpath(dirname(dirname(__FILE__))));
 
-require_once(WPU_TEST_ROOT_PATH . '/../../workspace/wordpress/wp-load.php');
+require_once('mockpress/mockpress.php');
 
 class WP_United_WPTest extends PHPUnit_Framework_TestCase {
 
@@ -17,10 +17,7 @@ class WP_United_WPTest extends PHPUnit_Framework_TestCase {
 	
 	
 	public function testBootWordPress() {
-		ob_start();
-		wp();
-		require_once( ABSPATH . WPINC . '/template-loader.php' );
-		ob_end_clean();
+		get_option('test');
 	}
 	
 	
