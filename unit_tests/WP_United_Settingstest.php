4c8bc6976208b5ed92f7cd35efb320011d8df6b8
John Wells <jfw@jfwhome.com>
2013-01-17 10:49:11 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1486 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- unit_tests/WP_United_Settingstest.php
+++ unit_tests/WP_United_Settingstest.php
@@ -3,28 +3,36 @@ define('WPU_TEST_ROOT_PATH', realpath(dirname(dirname(__FILE__))));
 
 class WP_United_SettingsTest extends PHPUnit_Framework_TestCase {
 
-	private $settings;
-
 	public function setup() {
 		define('IN_PHPBB', true);
 		require_once(WPU_TEST_ROOT_PATH . '/WordPress Plugin/wp-united/base-classes.php');
 	}
+	
+	
 	public function teardown() {}
+	
+	
+	
+	
 	
 	public function testSettingsObjectCreate() {
 		
-		$this->settings = WP_United_Settings::Create();
+		$settings = WP_United_Settings::Create();
 		
-		$this->assertTrue(is_object($this->settings));
+		$this->assertTrue($settings));
 	}
+	
+	
+	
+	
 	
 	/**
 	*	@depends testSettingsObjectCreate
 	*/
-	public function testSettingsSaveEmpty() {
+	public function testSettingsSaveEmpty($settings) {
 		
 		$emptyData = array();
-		$this->settings->update_settings($emptyData);
+		$settings->update_settings($emptyData);
 	}
 
 }
