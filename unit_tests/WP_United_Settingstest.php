ea35c82ce1ca3a5800d3136a4b87a8491a4579d1
John Wells <jfw@jfwhome.com>
2013-01-17 09:18:46 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1468 c1c82642-cce3-494f-bf0e-9d5eb4df7846

---
+++ unit_tests/WP_United_Settingstest.php
@@ -0,0 +1,23 @@
+<?
+
+
+
+class WP_United_SettingsTest extends PHPUnit_Framework_TestCase {
+
+	private $settings;
+
+	public function setup() {
+		define('IN_PHPBB', true);
+		require_once('../WordPress Plugin/wp-united/base-classes.php');
+	}
+	public function teardown() {}
+	
+	public function testSettingsObjectCreated() {
+		
+		$this->settings = WP_United_Settings::Create();
+		
+		$this->assertTrue(is_object($this->settings));
+	}
+
+
+}
