24543f637401885c2b6bdb5f4a504f1717d217c0
John Wells <jfw@jfwhome.com>
2013-01-17 11:00:06 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1491 c1c82642-cce3-494f-bf0e-9d5eb4df7846

---
+++ unit_tests/boot_test.php
@@ -0,0 +1,26 @@
+<?
+define('WPU_TEST_ROOT_PATH', realpath(dirname(dirname(__FILE__))));
+
+class WP_United_WPTest extends PHPUnit_Framework_TestCase {
+
+	public function setup() {
+		
+	}
+	
+	
+	public function teardown() {
+	
+		
+	}
+	
+	
+	public function testBootWordPress() {
+		ob_start();
+		require_once(WPU_TEST_ROOT_PATH . '../../workspace/wordpress/index.php');
+		ob_end_clean();
+	}
+	
+	
+	
+
+}
