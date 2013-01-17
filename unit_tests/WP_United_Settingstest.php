7a1ca383cac16e9c5cc0d31ae11548d00b779687
John Wells <jfw@jfwhome.com>
2013-01-17 10:51:53 +0000

    git-svn-id: http://www.wp-united.com/svn/trunk@1489 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- unit_tests/WP_United_Settingstest.php
+++ unit_tests/WP_United_Settingstest.php
@@ -19,7 +19,7 @@ class WP_United_SettingsTest extends PHPUnit_Framework_TestCase {
 		
 		$settings = WP_United_Settings::Create();
 		
-		$this->assertTrue(isObject($settings));
+		$this->assertTrue(is_object($settings));
 	}
 	
 	
