4567aab1b43e5839ba12e8673c6f3dce9aafce4c
John Wells <jfw@jfwhome.com>
2013-01-18 21:12:15 +0000

    new unit test for file parse errors, invluding all translations
    git-svn-id: http://www.wp-united.com/svn/trunk@1529 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- unit_tests/WP_United_Parsetest.php
+++ unit_tests/WP_United_Parsetest.php
@@ -20,22 +20,26 @@ class WP_United_ParseTest extends PHPUnit_Framework_TestCase {
 	public function test_phpbb() {
 		
 		$files = @$this->deep_glob(WPU_TEST_ROOT_PATH . '/phpbb3\ MOD/root/', '*.php', 0, -1);
-
+		$successLooksLike = sizeof($files);
+		$successIs = 0;
 		foreach($files as $file) { 
 			require_once($file);
+			$successIs++;
 		}
+		$this->assertTrue($successIs === $successLooksLike);
 		
 	}
 
 	public function test_wp() {
 		
 		$files = @$this->deep_glob(WPU_TEST_ROOT_PATH . '/WordPress\ Plugin/', '*.php', 0, -1);
-
-		foreach($files as $file) { echo '*';
-			if(stristr($file, 'wp-united.php') === false) {
-				require_once($file);
-			}
+		$successLooksLike = sizeof($files);
+		$successIs = 0;
+		foreach($files as $file) { 
+			require_once($file);
+			$successIs++;
 		}
+		$this->assertTrue($successIs === $successLooksLike);
 		
 	}
 	
