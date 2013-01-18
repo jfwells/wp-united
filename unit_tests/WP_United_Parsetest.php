5e8cb29457180a3a2278df854a96fff59eefd47a
John Wells <jfw@jfwhome.com>
2013-01-18 21:00:11 +0000

    new unit test for file parse errors, invluding all translations
    git-svn-id: http://www.wp-united.com/svn/trunk@1523 c1c82642-cce3-494f-bf0e-9d5eb4df7846

--- unit_tests/WP_United_Parsetest.php
+++ unit_tests/WP_United_Parsetest.php
@@ -17,7 +17,7 @@ class WP_United_ParseTest extends PHPUnit_Framework_TestCase {
 	
 	public function test_phpbb() {
 		
-		$files = @glob(WPU_TEST_ROOT_PATH . '/phpbb3 MOD/*.php');
+		$files = @$this->deep_glob(WPU_TEST_ROOT_PATH . '/phpbb3 MOD/', '*.php', 0, -1);
 
 		foreach($files as $file) { echo $file;
 			require_once($file);
@@ -27,7 +27,7 @@ class WP_United_ParseTest extends PHPUnit_Framework_TestCase {
 
 	public function test_wp() {
 		
-		$files = @glob(WPU_TEST_ROOT_PATH . '/WordPress Plugin/*.php');
+		$files = @$this->deep_glob(WPU_TEST_ROOT_PATH . '/WordPress Plugin/', '*.php', 0, -1);
 
 		foreach($files as $file) { echo $file;
 			if(stristr($file, 'wp-united.php') === false) {
@@ -36,5 +36,20 @@ class WP_United_ParseTest extends PHPUnit_Framework_TestCase {
 		}
 		
 	}
+	
+	private function deep_glob($path, $pattern = '*', $flags = 0, $depth = 0) {
+        $matches = array();
+        $folders = array(rtrim($path, DIRECTORY_SEPARATOR));
+       
+        while($folder = array_shift($folders)) {
+            $matches = array_merge($matches, glob($folder.DIRECTORY_SEPARATOR.$pattern, $flags));
+            if($depth != 0) {
+                $moreFolders = glob($folder.DIRECTORY_SEPARATOR.'*', GLOB_ONLYDIR);
+                $depth   = ($depth < -1) ? -1: $depth + count($moreFolders) - 2;
+                $folders = array_merge($folders, $moreFolders);
+            }
+        }
+        return $matches;
+    }
 
 }
